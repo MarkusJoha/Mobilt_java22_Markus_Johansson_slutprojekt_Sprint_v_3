09d22a04b56a5ad1a3845b4bd11e3f9ded714184
MarkusJoha <Markus.Johansson2@hotmail.com>
2023-10-12 12:37:11 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sprint_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sprint_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:168531300029:web:7848b544d77a20f860b1d4";
+  static String appKey = "AIzaSyDQJ1T24eyDv_UZ-4MNw_zVzgzJKSrVu9U";
+  static String messagingSenderId = "168531300029";
+  static String projectId = "chatappfirebase-723bd";
+  final primaryColor = const Color(0xFFee7b64);
+}
